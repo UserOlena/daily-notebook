a5da7bf6381c93ef5068562d3c3709443442f15b
Olena Pashchenko <helen.watermore@gmail.com>
2023-04-23 19:18:48 -0700

    Remove dotenv package, refactor.

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 const express = require('express');
@@ -45,8 +44,12 @@ app.post('/api/notes', (req, res) => {
                 res.sendStatus(500);
             } else {
                 const notes = JSON.parse(dbNotes);
-                const id = uuidv4();
-                const newNote = { title, text, id };
+                const newNote = { 
+                    title, 
+                    text, 
+                    id: uuidv4(),
+                };
+
                 notes.push(newNote);
 
                 writeToFile(notes, res);
